591c5fb52276f624e7867250a465e1a2b8cb76b1
rmucheri <vamucheri@gmail.com>
2024-07-10 01:06:29 -0600

    icon update

--- src/components/CauseHero.js
+++ src/components/CauseHero.js
@@ -2,7 +2,9 @@ import React from 'react';
 import xrays from '../assets/images/xrays.jpg';
 import charthos from '../assets/images/charthos.jpg';
 import docspatient from '../assets/images/docspatient.jpg';
-import { logo3 } from '../assets';
+import logo3 from '../assets/images/logo3.jpeg';
+import logo2 from '../assets/images/logo2.jpeg';
+import logo1 from '../assets/images/logo1.jpeg';
 // import { charthos } from '../assets';
 
 const causes = [
@@ -10,19 +12,22 @@ const causes = [
     title: "Comprehensive Cancer Care",
     subtitle: "Providing Hope and Healing Through Advanced Cancer Treatment",
     content: "At Karanda Mission Hospital, They are committed to offering comprehensive cancer care to economically disadvantaged patients. Your donations enable them to provide state-of-the-art treatments, including chemotherapy, radiation therapy, and surgical interventions. By supporting our cancer care program, you help them cover the cost of expensive medications and cutting-edge technologies that are essential for effective treatment. Additionally, they provide emotional and psychological support to patients and their families, ensuring they receive holistic care. Every contribution makes a significant difference, turning the tide against cancer and offering hope to those in desperate need.",
-    image: xrays // Ensure this image is properly imported
+    image: xrays, // Ensure this image is properly imported
+    icon: logo3
   },
   {
     title: "Community Outreach and Education",
     subtitle: "Empowering Communities Through Knowledge and Prevention",
     content: "Prevention is a powerful tool in the fight against cancer. At Karanda Mission Hospital, they believe in the importance of educating communities about cancer prevention, early detection, and healthy living. Your support allows them to conduct extensive outreach programs, including workshops, health fairs, and screening camps in rural areas. These initiatives help identify cancer at its earliest stages, increasing the chances of successful treatment. They also provide educational materials and resources to raise awareness about the risk factors and symptoms of cancer. Together, we can empower communities with the knowledge they need to prevent cancer and live healthier lives.",
-    image: charthos // Ensure this image is properly imported
+    image: charthos, // Ensure this image is properly imported
+    icon: logo3
   },
   {
     title: " Patient Support Services",
     subtitle: "Supporting Patients Every Step of the Way",
     content: "Cancer treatment can be a long and arduous journey. At Karanda Mission Hospital, They offer a range of support services to ensure patients and their families are not alone in this fight. Your donations help them provide transportation for patients traveling from remote areas, accommodation for those undergoing long-term treatment, and nutritional support to aid recovery. They also offer counseling and support groups to help patients cope with the emotional and psychological challenges of cancer. By contributing to their patient support services, you ensure that every patient receives the comprehensive care they need to navigate their treatment journey with dignity and hope.",
-    image: docspatient // Ensure this image is properly imported
+    image: docspatient, // Ensure this image is properly imported
+    icon: logo3
   }
 ];
 
@@ -44,7 +49,7 @@ function CauseHero() {
                 <div className="flex flex-col sm:flex-row mt-10">
                   <div className="sm:w-1/3 text-center sm:pr-8 sm:py-8">
                     <div className="w-20 h-20 rounded-full inline-flex items-center justify-center bg-gray-200 text-gray-400">
-                     <img src={logo3} />
+                     <img src={cause.icon} />
                     </div>
                     <div className="flex flex-col items-center text-center justify-center">
                       <h2 className="font-medium title-font mt-4 text-gray-900 text-lg">
