eb6f4b1c193637c33034a4362d2a91d36f63f348
rmucheri <vamucheri@gmail.com>
2024-07-27 01:32:28 -0600

    FOK

---
+++ src/components/Content/memberscontent.js
@@ -0,0 +1,25 @@
+const members = [
+    {
+        header: "Thomas Gondo",
+        text: "Thomas Gondo is a Licensed Land Surveyor in Ontario Canada and the President of Lejan Land Surveying Inc, a Licensed Practice also in the province of Ontario. He has consistently been involded in efforts to fundraise for Karanda Hospital."
+    },
+    {
+        header:"Victoria S Mamvura",
+      text: `"Victoria Siphiwe Mamvura holds a Masters, Post Graduate Diploma in HIV and AIDS, and Health Communication: Research, Monitoring & Evaluation of HIV & AIDS Projects, Developing Workplace Wellness Programs and Strategic Management in the field of HIV
+& AIDS. She has worked in different portfolios in Zimbabwe and Namibia working with organizations like University of Namibia, Bank Windhoek Namibia, Broadcasting Authority of Zimbabwe, Namibia Student Financial Assistance Fund amongst other organizations."`
+    },
+    {
+        header:"Blessing Mucherera",
+        text:"Blessing is a PhD researcher in Human Geography (Water Cultures), at the University of Hull, United Kingdom. With over 20 years of experience working with various charity organizations across Africa, the United Kingdom, and North America, Blessing brings a wealth of knowledge and expertise to our board. He is currently Chairperson, Bindura University of Science Education Member, Yorkshire and Humber Climate Commission (February 2022 - Present) Member, Hull Afro-Caribbean Association. Blessing's extensive experience in academia and his active involvement in climate and community organizations make him an invaluable asset to our team. His leadership and commitment to environmental and social causes align with our mission to create lasting positive change"
+    },
+    {
+        header:"Lindiwe Nyamukondiwa",
+        text:"Lindiwe is a registered nurse licensed in Alberta, Canada, holding a master’s degree in nursing. She brings twenty years of experience in geriatric nursing and is currently serving as a Clinical Manager at an organization in Calgary. Lindiwe is enthusiastic about joining the Friends of Karanda Mission Hospital board and is particularly eager to contribute to initiatives involving volunteer nurses through the 'Nurses Without Borders' program to support Karanda Hospital."
+        
+    },
+
+   
+   
+]
+
+export default members
