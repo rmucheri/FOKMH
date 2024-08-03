1341bc0fd7ea48048c537fb15e32f3726c677446
rmucheri <vamucheri@gmail.com>
2024-08-03 05:21:04 -0600

    links

--- src/components/Values.js
+++ src/components/Values.js
@@ -4,8 +4,8 @@ import values from './Content/valuecardcontent'
 
 function Values() {
   return (
-    <div className=''>
-        <h2 className="text-3xl text-center font-extrabold text-[#3ea498] mt-20">Our Vision</h2>
+    <div  id='values' className=''>
+        <h2 className="text-3xl text-center font-extrabold text-[#3ea498] mt-20">Our Values</h2>
     <div className='mx-auto grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-6 text-justify'>
     {
         values.map(value =>(
